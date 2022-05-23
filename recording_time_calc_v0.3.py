652e7b325b73caebd159a96f8cb607181b908efa
thom <hxy449999@gmail.com>
2022-05-23 14:18:58 +0800

    Tidy up code

--- recording_time_calc_v0.3.py
+++ recording_time_calc_v0.3.py
@@ -42,19 +42,15 @@ def add_new_data_set_to_cam_list(camera, new_data_set):
     camera.append(new_data_set)
 
 
+# Do I need this function? Not really.
 def name_generator(new_data_set):
     name = f"{new_data_set['resolution']}_{new_data_set['codec']}_{new_data_set['chroma subsampling']}_{new_data_set['bit depth']}_{new_data_set['frame rate']}"
     return name
 
 
-# filename = 'fx6_database.json'
-#
-# with open(filename, 'a') as f:
-#     json.dump(data_added, f)
-
-
 new_dict = generate_new_data_set('hd', 'xavcsi', 420, 8, 25, 122)
-name = name_generator(new_dict)
 fx6.append(new_dict)
-for i in fx6:
-    print(i, end='\n')
+
+filename = 'fx6_database.json'
+with open(filename, 'w') as f:
+    json.dump(fx6, f)
