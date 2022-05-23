f06e337221a2555f5d2c9a19ec386fe3b8ab045e
thom <hxy449999@gmail.com>
2022-05-23 15:52:02 +0800

    Finished write data to json functionality, and move the majority of code into functions, using functions to complete the task.

--- recording_time_calc_v0.3.py
+++ recording_time_calc_v0.3.py
@@ -1,28 +1,5 @@
 import json
 
-uhd_xavci_422_10_50 = {
-    'resolution': 'uhd',
-    'codec': 'xavc-i',
-    'chroma subsampling': 422,
-    'bit depth': 10,
-    'frame rate': 50,
-    'bitrate': 500,
-}
-
-uhd_xavci_422_10_25 = {
-    'resolution': 'uhd',
-    'codec': 'xavc-i',
-    'chroma subsampling': 422,
-    'bit depth': 10,
-    'frame rate': 25,
-    'bitrate': 250,
-}
-
-fx6 = [
-    uhd_xavci_422_10_50,
-    uhd_xavci_422_10_25,
-]
-
 
 def generate_new_data_set(resolution, codec, chroma_subsampling, bit_depth, frame_rate, bitrate):
     """generating a new set of data, return a dictionary for appending to the the camera list
@@ -38,19 +15,35 @@ def generate_new_data_set(resolution, codec, chroma_subsampling, bit_depth, fram
     return new_data_set
 
 
-def add_new_data_set_to_cam_list(camera, new_data_set):
-    camera.append(new_data_set)
+# # Do I need this function? Not really.
+# def name_generator(new_data_set):
+#     name = f"{new_data_set['resolution']}_{new_data_set['codec']}_{new_data_set['chroma subsampling']}_{new_data_set['bit depth']}_{new_data_set['frame rate']}"
+#     return name
+
+def get_from_json(file_path):
+    """Get json file from given file_path and return that file.
+    If given file_path doesn't exist, return None.
+    """
+    try:
+        with open(file_path, 'r') as f:
+            file = json.load(f)
+    except FileNotFoundError:
+        return None
+    else:
+        return file
 
 
-# Do I need this function? Not really.
-def name_generator(new_data_set):
-    name = f"{new_data_set['resolution']}_{new_data_set['codec']}_{new_data_set['chroma subsampling']}_{new_data_set['bit depth']}_{new_data_set['frame rate']}"
-    return name
+def write_to_json(file_path, new_dict):
+    """Using get_from_json() to get file (list), then append given new_dict to that file (list).
+    Finally, overwriting that file with new list.
+    """
+    fx6 = get_from_json(file_path)
+    fx6.append(new_dict)
+    with open(file_path, 'w') as f:
+        json.dump(fx6, f)
 
-
-new_dict = generate_new_data_set('hd', 'xavcsi', 420, 8, 25, 122)
-fx6.append(new_dict)
 
 filename = 'fx6_database.json'
-with open(filename, 'w') as f:
-    json.dump(fx6, f)
+
+n_d = generate_new_data_set('uhd', 'xavcl', 420, 8, 50, 150)
+write_to_json(filename, n_d)
---
+++ test.py
@@ -0,0 +1,13 @@
+x = [{"resolution": "uhd", "codec": "xavci", "chroma subsampling": 422, "bit depth": 10, "frame rate": 50,
+      "bitrate": 500},
+     {"resolution": "hd", "codec": "xavci", "chroma subsampling": 422, "bit depth": 10, "frame rate": 25,
+      "bitrate": 128},
+     {"resolution": "hd", "codec": "xavcsi", "chroma subsampling": 422, "bit depth": 10, "frame rate": 50,
+      "bitrate": 128},
+     {"resolution": "hd", "codec": "xavcsi", "chroma subsampling": 422, "bit depth": 10, "frame rate": 50,
+      "bitrate": 128},
+     {"resolution": "uhd", "codec": "xavcl", "chroma subsampling": 420, "bit depth": 8, "frame rate": 50,
+      "bitrate": 150}]
+
+for i in x:
+    print(i, end='\n')
